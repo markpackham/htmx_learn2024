9b07ef72f3fd83ecbf9b4d1fcf2d0bea0dfb3394
mark <markpackham1@gmail.com>
2024-01-02 10:38:15 +0000

    Rename index file to request.html
    https://youtu.be/0UvA7zvwsmg?si=DEUugneiOZEheXnf&t=1418

--- server.js
+++ server.js
@@ -19,15 +19,23 @@ app.get("/users", async (req, res) => {
   //     { id: 3, name: "Shannon Jackson" },
   //   ];
 
-  const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
-  const users = await response.json();
+  setTimeout(async () => {
+    // Limit sent via "hx-vals", use 10 if not sent
+    // use +req to convert string to number
+    const limit = +req.query.limit || 10;
 
-  res.send(`
-  <h1 class="text-2xl font-bold my-4">Users</h1>
-  <ul>
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users?_limit=${limit}`
+    );
+    const users = await response.json();
+
+    res.send(`
+    <h1 class="text-2xl font-bold my-4">Users</h1>
+    <ul>
       ${users.map((user) => `<li>${user.name}</li>`).join("")}
     </ul>
   `);
+  }, 2000);
 });
 
 // Start server
