3ab90f5251131a1f2a52dbcdd8353ae936696bbb
mark <markpackham1@gmail.com>
2024-01-02 09:24:35 +0000

    Output users
    https://youtu.be/0UvA7zvwsmg?feature=shared&t=961

--- server.js
+++ server.js
@@ -9,6 +9,24 @@ app.use(experess.urlencoded({ extended: true }));
 // Parse JSON bodies (as sent by API clients)
 app.use(experess.json());
 
+// Routes
+
+// Fetch users
+app.get("/users", (req, res) => {
+  const users = [
+    { id: 1, name: "John Doe" },
+    { id: 2, name: "Bob Williams" },
+    { id: 3, name: "Shannon Jackson" },
+  ];
+
+  res.send(`
+  <h1 class="text-2xl font-bold my-4">Users</h1>
+  <ul>
+      ${users.map((user) => `<li>${user.name}</li>`).join("")}
+    </ul>
+  `);
+});
+
 // Start server
 // http://localhost:3000/
 app.listen(3000, () => {
