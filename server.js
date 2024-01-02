fb04267e00c1015c718e84e1f8c917eaf39786f7
mark <markpackham1@gmail.com>
2024-01-02 10:04:22 +0000

    Grab users from jsonplaceholder
    https://youtu.be/0UvA7zvwsmg?si=QM7ehNcYf1yAQ-64&t=1111

--- server.js
+++ server.js
@@ -12,12 +12,15 @@ app.use(experess.json());
 // Routes
 
 // Fetch users
-app.get("/users", (req, res) => {
-  const users = [
-    { id: 1, name: "John Doe" },
-    { id: 2, name: "Bob Williams" },
-    { id: 3, name: "Shannon Jackson" },
-  ];
+app.get("/users", async (req, res) => {
+  //   const users = [
+  //     { id: 1, name: "John Doe" },
+  //     { id: 2, name: "Bob Williams" },
+  //     { id: 3, name: "Shannon Jackson" },
+  //   ];
+
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const users = await response.json();
 
   res.send(`
   <h1 class="text-2xl font-bold my-4">Users</h1>
